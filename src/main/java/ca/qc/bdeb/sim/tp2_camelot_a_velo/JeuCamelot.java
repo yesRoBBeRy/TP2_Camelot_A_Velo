677d63aeffb290b52b5bab442ecf4ddcabacca89
2467523 <2467523@bdeb.qc.ca>
2025-11-11 10:21:16 -0500

    changed HelloApplication.java to JeuCamelot.java

--- src/main/java/ca/qc/bdeb/sim/tp2_camelot_a_velo/JeuCamelot.java
+++ src/main/java/ca/qc/bdeb/sim/tp2_camelot_a_velo/JeuCamelot.java
@@ -1,4 +1,24 @@
 package ca.qc.bdeb.sim.tp2_camelot_a_velo;
 
-public class JeuCamelot {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class JeuCamelot extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
