b918cf7a4f3bb230041d16b56f94049214871623
Noam <2467523@bdeb.qc.ca>
2025-11-21 14:01:16 -0500

    Changes by 6303072, camera suit maintenant correctement le camelot

--- src/main/java/ca/qc/bdeb/sim/tp2_camelot_a_velo/Camera.java
+++ src/main/java/ca/qc/bdeb/sim/tp2_camelot_a_velo/Camera.java
@@ -1,4 +1,29 @@
 package ca.qc.bdeb.sim.tp2_camelot_a_velo;
 
-public class Camera {
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+public class Camera{
+    private Point2D positionCamera;
+
+    public Camera(Point2D positionCamera){
+        this.positionCamera = positionCamera;
+    }
+    /**
+     * Fait avancer la caméra vers la droite automatiquement
+     */
+    public void update(double deltaTemps, Camelot camelot) {
+        double cibleX = camelot.getPosition().getX() - JeuCamelot.largeur * 0.2;
+        positionCamera = new Point2D(positionCamera.getX() + (cibleX - positionCamera.getX()),0);
+        System.out.println(positionCamera);
+    }
+
+    public Point2D coordoEcran(Point2D positionMonde){
+        return positionMonde.subtract(positionCamera);
+
+    }
+
+    public Point2D getPositionCamera() {
+        return positionCamera;
+    }
 }
